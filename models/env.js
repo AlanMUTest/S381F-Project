3f18c708b6b653b18383c4fb899cb122d325a779
AlanMUTest <150664556+AlanMUTest@users.noreply.github.com>
2023-11-23 21:23:40 +0800

    Create env.js

---
+++ models/env.js
@@ -0,0 +1,9 @@
+const MONGO_CONNECTION_URL='mongodb+srv://test:UfqzGEnZMi2F3bFQ@s381f-miniproj.tcivx42.mongodb.net/?retryWrites=true&w=majority';
+const SESSION_SECRET = 'test_session';
+const COOKIE_MAXAGE = 30 * 60; // 30 minutes
+
+module.exports = {
+    MONGO_CONNECTION_URL,
+    SESSION_SECRET,
+    COOKIE_MAXAGE
+}
